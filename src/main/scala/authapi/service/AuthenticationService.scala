14c331e0d448a9bd9a98e64b2171d5b30a9b9a3e
ewan.squires <ewan.squires@gamesys.co.uk>
2020-04-28 10:46:12 +0100

    Created admin authorization check on user/all.

--- src/main/scala/authapi/service/AuthenticationService.scala
+++ src/main/scala/authapi/service/AuthenticationService.scala
@@ -3,7 +3,7 @@ package authapi.service
 import authapi.model.User
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import play.api.libs.json.{JsObject, JsString, Json}
+import play.api.libs.json.{JsNumber, JsObject, JsString, Json}
 
 @Service
 class AuthenticationService @Autowired()(private val jwtService: JwtService, private val userService: UserService) {
@@ -12,19 +12,17 @@ class AuthenticationService @Autowired()(private val jwtService: JwtService, pri
     if (isRequesterAuthentic(user)) {
       jwtService.createToken(user)
     } else {
-      generateJsonMessage("Access Denied.")
+      "\"message\":\"Access Denied.\""
     }
   }
 
-  private def isRequesterAuthentic(user: User) = ! (userService.findUser(user.getId()).get().getRole() == "Banned")
+  def isAdmin(token: String): Boolean = isRole("Admin", token)
 
-  private def generateJsonMessage(message: String) = {
-    Json.stringify(
-      JsObject(
-        Seq(
-          "message" -> JsString(message)
-        )
-      )
-    )
+  private def isRole(role: String, token: String): Boolean = {
+    val payload = Json.parse(jwtService.getMessageFromToken(token))
+    val tokenRole = (payload \ "role").get.toString().replace("\"", "")
+    tokenRole.equals(role)
   }
+
+  private def isRequesterAuthentic(user: User) = ! (userService.findUser(user.getId()).get().getRole() == "Banned")
 }
